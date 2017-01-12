24bec081b30fcde13868bec27906c6e00049650e
nlovas <nlovas@ualberta.ca>
2017-01-11 18:03:34 -0700

    updated python code

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 import requests
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/nlovas/cmput404lab1/master/version.py")
 
 print response.text
 print response.status_code
